8e7bc69176062f533a2df5c5e07398d8bb7eb819
realalexis <alexismoulliere44@gmail.com>
2024-02-14 13:47:38 +0100

    nom de variable clean

--- resoc_n1/Niveau1/scripts/suppressionArticle.php
+++ resoc_n1/Niveau1/scripts/suppressionArticle.php
@@ -7,31 +7,31 @@ include 'connexion.php';
 if (isset($_GET['post_id'])) {
     $postId = intval($_GET['post_id']);
 
-    $deleteQuery = "DELETE FROM likes WHERE post_id = ? ";
-    $deleteStmt = $mysqli->prepare($deleteQuery);
-    $deleteStmt->bind_param("i", $postId);
-    $deleteStmt->execute();
-    if ($deleteStmt->affected_rows > 0) {
+    $deleteLikesQuery = "DELETE FROM likes WHERE post_id = ? ";
+    $deleteLikesStmt = $mysqli->prepare($deleteLikesQuery);
+    $deleteLikesStmt->bind_param("i", $postId);
+    $deleteLikesStmt->execute();
+    if ($deleteLikesStmt->affected_rows > 0) {
         echo "Les likes ont bien été supprimés";
     } else {
         echo "Erreur lors de la suppression des likes";
     }
 
-    $deleteQueryTag = "DELETE FROM posts_tags WHERE post_id = ? ";
-    $deleteTags = $mysqli->prepare($deleteQueryTag);
-    $deleteTags->bind_param("i", $postId);
-    $deleteTags->execute();
+    $deleteTagsQuery = "DELETE FROM posts_tags WHERE post_id = ? ";
+    $deleteTagsStmt = $mysqli->prepare($deleteTagsQuery);
+    $deleteTagsStmt->bind_param("i", $postId);
+    $deleteTagsStmt->execute();
     if ($deleteTags->affected_rows > 0) {
         echo "Les tags ont bien été supprimés";
     } else {
         echo "Erreur lors de la suppression des tags";
     }
 
-    $deleteQueryPost = "DELETE FROM posts WHERE id = ? ";
-    $deletePosts = $mysqli->prepare($deleteQueryPost);
-    $deletePosts->bind_param("i", $postId);
-    $deletePosts->execute();
-    if ($deletePost->affected_rows > 0) {
+    $deletePostsQuery = "DELETE FROM posts WHERE id = ? ";
+    $deletePostsStmt = $mysqli->prepare($deletePostsQuery);
+    $deletePostsStmt->bind_param("i", $postId);
+    $deletePostsStmt->execute();
+    if ($deletePostsStmt->affected_rows > 0) {
         echo "Le post a bien été supprimé";
     } else {
         echo "Erreur lors de la suppression du post";
